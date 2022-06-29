c464fcd988fda022d66f60c57699c37d450c35fc
TAK4129 <e1317104@oit.ac.jp>
2022-06-29 20:14:30 +0900

    master

--- Libs/TrackRadarLib2.lua
+++ Libs/TrackRadarLib2.lua
@@ -23,13 +23,13 @@ TrackingRadar = {
 	---@param ch_out_v number
 	---@param ch_out_fov number
 	---@return TrackingRadar
-	new = function(cls,ch_in_h, ch_in_v, ch_out_h, ch_out_v, ch_out_fov)
+	new = function(cls, ch_in_h, ch_in_v, ch_out_h, ch_out_v, ch_out_fov)
 		local obj = LifeBoatAPI.lb_copy(cls,
-		{ch_in_h=ch_in_h,
-		ch_in_v=ch_in_v,
-		ch_out_h=ch_out_h,
-		ch_out_v=ch_out_v,
-		ch_out_fov=ch_out_fov})
+			{ ch_in_h = ch_in_h,
+				ch_in_v = ch_in_v,
+				ch_out_h = ch_out_h,
+				ch_out_v = ch_out_v,
+				ch_out_fov = ch_out_fov })
 		return obj
 	end;
 
@@ -37,7 +37,7 @@ TrackingRadar = {
 	---@param self TrackingRadar
 	---@return boolean horizontal, boolean vertical
 	isTracking = function(self)
-		return input.getNumber(self.ch_in_h)~=0, input.getNumber(self.ch_in_v)~=0
+		return input.getNumber(self.ch_in_h) ~= 0, input.getNumber(self.ch_in_v) ~= 0
 	end;
 	---@endsection
 
@@ -46,9 +46,9 @@ TrackingRadar = {
 	---@param x number
 	---@param y number
 	---@param z number
-	setViewFromPos = function(self, x,y,z)
-		output.setNumber(self.ch_out_h,math.atan(x,z))
-		output.setNumber(self.ch_out_v,math.atan(y-0.5,math.sqrt(z^2*x^2)))
+	setViewFromPos = function(self, x, y, z)
+		output.setNumber(self.ch_out_h, math.atan(x, z))
+		output.setNumber(self.ch_out_v, math.atan(y - 0.5, math.sqrt(z ^ 2 * x ^ 2)))
 	end;
 	---@endsection
 
@@ -57,20 +57,20 @@ TrackingRadar = {
 	---@param elevation number
 	---@param azimuth number
 	setViewFromAngle = function(self, azimuth, elevation)
-		output.setNumber(self.ch_out_h,azimuth)
-		output.setNumber(self.ch_out_v,elevation)
+		output.setNumber(self.ch_out_h, azimuth)
+		output.setNumber(self.ch_out_v, elevation)
 	end;
 	---@endsection
 
 	---@section getPOS
 	---@param self TrackingRadar
-	getPos=function(self)
-		if not self:isTracking() then return 0,0,0 end
-		local x,y,z,xz
-		y,xz=input.getNumber(self.ch_in_h+2)*math.sin(input.getNumber(self.ch_in_h+1)*2*math.pi), input.getNumber(self.ch_in_h+2)*math.cos(input.getNumber(self.ch_in_h+1)*2*math.pi)
-		x=xz*math.cos(input.getNumber(self.ch_in_v+1)*2*math.pi)
-		z=xz*math.sin(input.getNumber(self.ch_in_v+1)*2*math.pi)
-		return x,y,z
+	getPos = function(self)
+		if not self:isTracking() then return 0, 0, 0 end
+		local x, y, z, xz
+		y, xz = input.getNumber(self.ch_in_h + 2) * math.sin(input.getNumber(self.ch_in_h + 1) * 2 * math.pi), input.getNumber(self.ch_in_h + 2) * math.cos(input.getNumber(self.ch_in_h + 1) * 2 * math.pi)
+		x = xz * math.cos(input.getNumber(self.ch_in_v + 1) * 2 * math.pi)
+		z = xz * math.sin(input.getNumber(self.ch_in_v + 1) * 2 * math.pi)
+		return x, y, z
 	end
 	---@endsection
 }
--- test.lua
+++ test.lua
@@ -1,4 +1,4 @@
-require("Matrix")
+require("Libs.LightMatrix")
 
 function onTick()
 	local pitch = input.getNumber(1)
@@ -8,14 +8,14 @@ function onTick()
 	local elev = input.getNumber(4)
 	local azim = input.getNumber(5)
 
-	local mat = Matrix.new(3,1)
+	local mat = LMatrix:new(3,1)
 	mat:set(2,1,math.sin(elev*math.pi/2))
 	local xz = math.cos(elev*math.pi/2)
 	mat:set(1,1,xz*math.sin(azim*math.pi/2))
 	mat:set(3,1,xz*math.cos(azim*math.pi/2))
 
-	local r = Matrix.rm(roll,pitch,yaw)
-	local matr = r:inv():dot(mat)
+	local r = LMatrix:newRotateMatrix(roll,pitch,yaw)
+	local matr = r:solve(mat)
 	local ad = math.atan(matr:get(1,1),matr:get(3,1))
 	local xzd = math.sqrt(matr:get(3,1)^2+matr:get(1,1)^2)
 	local ed = math.atan(matr:get(2,1),xzd)
