308ce6833534d1802176110764c920e010d82538
TAK4129 <e1317104@oit.ac.jp>
2022-07-05 23:37:13 +0900

    master

--- tracking_test.lua
+++ tracking_test.lua
@@ -4,8 +4,8 @@ require("LifeBoatAPI.Maths.LBMaths")
 require("Libs.PID")
 
 RADAR = TrackingRadar:new(7, 4, 6, 5, 4)
-OFFSET_RADAR = LMatrix:newFromArray({ 0, 0.25, 0 }, 3, 1)
-PivotPID = PID:new(1, 0.0001, 0.2, 0.3)
+OFFSET_RADAR = LMatrix:newFromArray({ 0, -0.25, 0 }, 3, 1)
+PivotPID = PID:new(4.5, 0, 0.5, 0.3)
 
 function onTick()
 	local params = {}
@@ -15,15 +15,16 @@ function onTick()
 	RADAR:trackingUpdate()
 	local pi2 = math.pi * 2
 	--local Rpitch = (math.asin(math.sin(params[14] * pi2) / math.cos(params[15] * pi2)) - math.asin(math.sin(params[12] * pi2) / math.cos(params[15] * pi2))) / pi2
-	local pos = LMatrix:newFromArray({1,0,1}, 3, 1)
+	local pos = LMatrix:newFromArray(RADAR:getPos(), 3, 1)
 	local rotationRadar = LMatrix:newRotateMatrix(params[14], params[15], params[17])
 	local rotationBase = LMatrix:newRotateMatrix(params[10], params[11], params[13])
-	local posout = rotationRadar:dot(pos)
-	local a, e = rotationBase:solve(pos):getAngle()
+	local posout = rotationRadar:dot(pos):sub(rotationRadar:dot(OFFSET_RADAR))
+	--debug.log("TST/ "..posout:get(1,1).." , "..posout:get(2,1).." , "..posout:get(3,1))
+	local a, e = rotationBase:solve(posout):getAngle()
 	local isTracking_h, isTracking_v = RADAR:isTracking()
 
-	if true then
+	if isTracking_h and isTracking_v then
 		output.setNumber(7, 2 * e / math.pi)
-		output.setNumber(8, PivotPID:update((a / pi2 + params[18] + 1.5) % 1 - 0.5, 0))
+		output.setNumber(8, PivotPID:update((-params[13]+params[18] + a / pi2 + 1.5) % 1 - 0.5, 0))
 	end
 end
