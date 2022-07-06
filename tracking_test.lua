543d16df41397942f5a6d6e32356d37796f6dc1c
TAK <ekxmekxm@gmail.com>
2022-07-06 12:36:00 +0900

    master

--- tracking_test.lua
+++ tracking_test.lua
@@ -25,6 +25,6 @@ function onTick()
 
 	if isTracking_h and isTracking_v then
 		output.setNumber(7, 2 * e / math.pi)
-		output.setNumber(8, PivotPID:update((-params[12] + a / pi2 + 1.5) % 1 - 0.5, 0))
+		output.setNumber(8, PivotPID:update((a / pi2 - params[12] + 1.5) % 1 - 0.5, 0))
 	end
 end
