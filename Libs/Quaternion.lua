43fc52b8fdaab2090c13588b15149d3aec6cedc1
TAK <ekxmekxm@gmail.com>
2022-07-08 17:21:05 +0900

    master

--- Libs/Quaternion.lua
+++ Libs/Quaternion.lua
@@ -12,6 +12,36 @@ Quaternion = {
 		return LifeBoatAPI.lb_copy(cls,{Q=q})
 	end;
 
+	
+	---@section _getConjugateQuaternion
+	---@param self Quaternion
+	---@return Quaternion
+	_getConjugateQuaternion = function(self)
+		local c = self:_new()
+			for i = 1, 3 do
+				c.Q[i] = -self.Q[i]
+			end
+			c.Q[4] = -self.Q[4]
+		return c
+	end;
+	---@endsection
+
+	---@section _product calculate A⊗B
+	---@param self Quaternion A
+	---@param target Quaternion B
+	---@return Quaternion
+	_product = function(self, target)
+		local a = self
+		local result = Quaternion:_new()
+		for i = 1, 4 do
+			for j = 1, 4 do
+				
+			end
+		end
+		return result
+	end;
+	---@endsection
+
 	---@section newRotateQuaternion
 	---@param cls Quaternion
 	---@param angle number Turn(0 to 1, correspond to 0 to 2π)
@@ -29,12 +59,14 @@ Quaternion = {
 	---@endsection
 
 
+	--[[
 	---@section rotateVector
 	---@param self Quaternion
 	---@param vector table
 	---@return table
 	rotateVector = function(self, vector)
 
-	end
+	end;
 	---@endsection
+	]]
 }
